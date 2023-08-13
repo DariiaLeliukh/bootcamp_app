e17836170e6282817963f968740d4a63e24472cf
DariiaLeliukh <dariialelyukh@gmail.com>
2023-08-12 17:28:57 -0700

    Add select for teacher assistance for a cohort

---
+++ teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx',
+  port: 5432
+});
+
+const arguments = process.argv.slice(2, process.argv.length);
+let requestedCohort = arguments[0];
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON assistance_requests.teacher_id = teachers.id
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = '${requestedCohort || 'JUL02'}'
+ORDER BY teacher;`)
+  .then(res => {
+    console.log(res.rows);
+  })
+  .catch(err => console.error('query error', err.stack));
