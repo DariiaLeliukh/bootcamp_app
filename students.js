80c70b615308877236be178a31a3587916948e26
DariiaLeliukh <dariialelyukh@gmail.com>
2023-08-12 17:14:00 -0700

    Modify the file to accept arguments for select query

--- students.js
+++ students.js
@@ -8,13 +8,31 @@ const pool = new Pool({
   port: 5432
 });
 
+const arguments = process.argv.slice(2, process.argv.length);
 
-pool.query(`
+if (arguments.length === 2) {
+  let requestedCohort = arguments[0];
+  let limit = arguments[1];
+  pool.query(`
+  SELECT students.id, students.name as student_name, cohorts.name as cohort_name 
+  FROM students 
+  JOIN cohorts ON cohorts.id = students.cohort_id 
+  WHERE cohorts.name LIKE '%${requestedCohort}%'
+  LIMIT ${limit};`)
+    .then(res => {
+      console.log(res.rows);
+    })
+    .catch(err => console.error('query error', err.stack));
+} else if (arguments.length > 0 && arguments.length !== 2) {
+  console.log('Too many or too little arguments');
+} else {
+  pool.query(`
 SELECT students.id, students.name as student_name, cohorts.name as cohort_name 
 FROM students 
 JOIN cohorts ON cohorts.id = students.cohort_id 
 LIMIT 5;`)
-  .then(res => {
-    console.log(res.rows);
-  })
-  .catch(err => console.error('query error', err.stack));
+    .then(res => {
+      console.log(res.rows);
+    })
+    .catch(err => console.error('query error', err.stack));
+}
